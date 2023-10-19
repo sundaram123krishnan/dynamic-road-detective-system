abf50a269ea0ef5f6fcbed91640dcfa01d053daa
sundaram123krishnan <krishnsundaram@gmail.com>
2023-10-19 21:47:02 +0530

    fixing cors issue

--- backend/vpc-peering/handler.js
+++ backend/vpc-peering/handler.js
@@ -22,6 +22,10 @@ exports.handler = async (event) => {
 
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+      },
       body: JSON.stringify({ message: 'Location saved successfully' }),
     };
   } catch (error) {
