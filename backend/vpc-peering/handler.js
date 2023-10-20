ec26cbe7b3145c44c5e13c003368b64bd4d316ce
sundaram123krishnan <krishnsundaram@gmail.com>
2023-10-20 16:30:55 +0530

    cors

--- backend/vpc-peering/handler.js
+++ backend/vpc-peering/handler.js
@@ -36,3 +36,16 @@ exports.handler = async (event) => {
     };
   }
 };
+
+module.exports.options = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+      'Access-Control-Allow-Methods': 'OPTIONS,POST',
+    },
+  };
+
+  callback(null, response);
+};
