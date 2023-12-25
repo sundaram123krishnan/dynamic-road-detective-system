a5ad9df527ae6559b6d2dc17e3ba421e8668c6c0
sundaram123krishnan <krishnsundaram@gmail.com>
2023-12-25 18:18:23 +0530

    removing white text

--- next-frontend/src/app/components/Heading.tsx
+++ next-frontend/src/app/components/Heading.tsx
@@ -1,6 +1,7 @@
-
 export default function Heading() {
-    return (
-        <div className="text-3xl font-bold text-white text-center uppercase">Dynamic road detective system</div>
-    )
+  return (
+    <div className="text-3xl font-bold text-center uppercase">
+      Dynamic road detective system
+    </div>
+  );
 }
