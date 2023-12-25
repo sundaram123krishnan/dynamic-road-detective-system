68d4d8ae345232d2eda28a9cfda9b6599f8d8b1e
sundaram123krishnan <krishnsundaram@gmail.com>
2023-12-25 18:16:28 +0530

    adding api key

--- next-frontend/next.config.js
+++ next-frontend/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        apiKey: 'AIzaSyDkRb7OLdil8cgM2b5ZRvbbJYzZZvtNBBE',
-    }
-}
+  env: {
+    apiKey: "AIzaSyDkRb7OLdil8cgM2b5ZRvbbJYzZZvtNBBE",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
