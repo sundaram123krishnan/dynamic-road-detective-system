447b683bb0749fa927ac8375db584e063c4302d0
sundaram123krishnan <krishnsundaram@gmail.com>
2023-12-26 18:33:01 +0530

    displaying msg during loading using shadcn ui lib

--- next-frontend/src/app/loading.tsx
+++ next-frontend/src/app/loading.tsx
@@ -1,3 +1,15 @@
+import { Terminal } from "lucide-react";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
 export default function Loading() {
-    return <div>Loading...</div>
+  return (
+    <Alert className="border-blue-500 bg-blue-100">
+      <Terminal className="h-4 w-4" />
+      <AlertTitle className="font-bold">Alert!</AlertTitle>
+      <AlertDescription>
+        Please enable location on your device to use this application
+      </AlertDescription>
+    </Alert>
+  );
 }
