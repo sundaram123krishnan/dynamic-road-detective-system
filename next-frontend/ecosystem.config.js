e6aca33c322c2390b407bcdf6362e208275d60d4
Your Name here only <krishnsundaram@gmail.com>
2024-01-06 18:44:32 +0530

    added ecosystem

---
+++ next-frontend/ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [
+    {
+      script: "npm run dev",
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: "key.pem",
+      user: "ubuntu",
+      host: "13.201.13.158",
+      ref: "origin/master",
+      repo: "https://github.com/sundaram123krishnan/dynamic-road-detective-system",
+      path: "/home/ubuntu",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+      ssh_options: "ForwardAgent=yes",
+    },
+  },
+};
